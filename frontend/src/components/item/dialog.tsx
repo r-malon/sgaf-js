860309544347b28e133ac1e7969cb66a9ea28991
usuario <33752131+r-malon@users.noreply.github.com>
2025-09-24 11:55:17 -0300

    Remove dead code and add valor label

--- frontend/src/components/item/dialog.tsx
+++ frontend/src/components/item/dialog.tsx
@@ -43,7 +43,7 @@ export function ItemDialog({
       }}
       fields={[
         { name: 'descricao', label: 'Descrição', type: 'textarea' },
-        { name: 'valor', type: 'money' },
+        { name: 'valor', label: 'Valor mensal', type: 'money' },
         { name: 'banda_maxima', label: 'Banda Máxima', type: 'number' },
         { name: 'banda_instalada', label: 'Banda Instalada', type: 'number' },
         { name: 'data_instalacao', label: 'Data de Instalação', type: 'date' },
